45a95b92a25b09a5a8f079ad8adf7bac87bf0a4b
cristina-rodean <cristina.rodean123@gmail.com>
2023-04-18 20:22:38 +0300

    Regular files done

--- amazingProject.c
+++ amazingProject.c
@@ -3,7 +3,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
+#include <string.h>
 
 
 int FileType(char *path, struct stat *buff){
@@ -26,8 +26,37 @@ int FileType(char *path, struct stat *buff){
     return -1;
 }
 
-int checkPathOfFile(char *path){
+void menuForRegular(char *filename){
     
+    
+    printf("---- MENU ----\n");
+    printf("* n: name\n");
+    printf("* m: last time of modif\n");
+    printf("* a: access\n");
+     
+    printf("\nPlease enter your options\n\n");
+    printf("STANDARD INPUT: ");
+
+    char input[10];
+    struct stat buff;
+    fgets(input, 10, stdin);
+    
+    for(int i=1;i<strlen(input)-1; i++){
+        if(input[i]=='n')
+            printf("Name of file:%s\n", filename );
+        else
+            if(input[i]=='d')
+              printf("Size: %d bytes\n", (int)buff.st_size);
+            else
+               if(input[i]=='h')    
+                  printf("Hard links count: %d\n", (int)buff.st_size);
+               else
+                  if(input[i]=='m')
+                     printf("Time of last modification: %ld\n", buff.st_mtime);
+                     
+    }
+
+
 }
 
 int main(int argc, char **argv){
@@ -40,15 +69,16 @@ int main(int argc, char **argv){
             int typeOfFile=FileType(argv[i], buff);
 
             if (typeOfFile==0){
-                printf("REGULAR FILE");
+                printf("REGULAR FILE\n");
+                menuForRegular(argv[i]);
             }
 
             if (typeOfFile==1){
-                printf("DIRECTORY");
+                printf("DIRECTORY\n");
             }
 
             if (typeOfFile==2){
-                printf("SYMBOLIC LINK");
+                printf("SYMBOLIC LINK\n");
             }
         }
     }
